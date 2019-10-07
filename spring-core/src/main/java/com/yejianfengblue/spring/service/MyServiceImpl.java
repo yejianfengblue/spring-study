55141213b4dd6c35f9a0dea1b67d706280044639
Jian Feng Ye <yejianfengblue@163.com>
2019-10-07 23:11:40 +0800

    Introduce the JSR-250 @PostConstruct and @PreDestory annotations which receive lifecycle callbacks

--- spring-core/src/main/java/com/yejianfengblue/spring/service/MyServiceImpl.java
+++ spring-core/src/main/java/com/yejianfengblue/spring/service/MyServiceImpl.java
@@ -1,11 +1,23 @@
 package com.yejianfengblue.spring.service;
 
 import java.time.LocalDateTime;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 public class MyServiceImpl implements MyService {
 
     @Override
     public void print() {
         System.out.println("MyServiceImpl.print() at " + LocalDateTime.now().toString());
+    }
+
+    @PostConstruct
+    public void postConstructPrint() {
+        System.out.println("MyServiceImpl.postConstructPrint() at " + LocalDateTime.now().toString());
+    }
+
+    @PreDestroy
+    public void preDestroyPrint() {
+        System.out.println("MyServiceImpl.preDestroyPrint() at " + LocalDateTime.now().toString());
     }
 }
