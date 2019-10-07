1412f1de7c6af0bfbf1024d8f808bb8cc7c3143c
Jian Feng Ye <yejianfengblue@163.com>
2019-10-07 22:29:19 +0800

    Register a shutdown hook to gracefully shut down the Spring IoC container

--- spring-core/src/main/java/com/yejianfengblue/spring/Main.java
+++ spring-core/src/main/java/com/yejianfengblue/spring/Main.java
@@ -2,14 +2,20 @@ package com.yejianfengblue.spring;
 
 import com.yejianfengblue.spring.config.AppConfig;
 import com.yejianfengblue.spring.service.MyService;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 class Main {
 
     public static void main(String [] args) {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        /*
+        In a non-web application, registering a shutdown hook with JVM ensures a graceful shutdown
+        and calls the relevant destroy methods on the singleton beans.
+        https://docs.spring.io/spring-framework/docs/current/spring-framework-reference/core.html#beans-factory-shutdown
+         */
+        context.registerShutdownHook();
         MyService myService = context.getBean(MyService.class);
         myService.print();
     }
