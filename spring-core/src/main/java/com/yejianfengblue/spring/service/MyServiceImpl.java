6c89a685be3fa8e67f5748db21dc4d00bbbd1315
Jian Feng Ye <yejianfengblue@163.com>
2019-10-12 21:01:49 +0800

    Configure Log4j2 in Spring Core

--- spring-core/src/main/java/com/yejianfengblue/spring/service/MyServiceImpl.java
+++ spring-core/src/main/java/com/yejianfengblue/spring/service/MyServiceImpl.java
@@ -1,23 +1,28 @@
 package com.yejianfengblue.spring.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.time.LocalDateTime;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 public class MyServiceImpl implements MyService {
 
+    private Logger log = LoggerFactory.getLogger(getClass());
+
     @Override
     public void print() {
-        System.out.println("MyServiceImpl.print() at " + LocalDateTime.now().toString());
+        log.info("MyServiceImpl.print() at " + LocalDateTime.now().toString());
     }
 
     @PostConstruct
     public void postConstructPrint() {
-        System.out.println("MyServiceImpl.postConstructPrint() at " + LocalDateTime.now().toString());
+        log.trace("MyServiceImpl.postConstructPrint() at {}", LocalDateTime.now().toString());
     }
 
     @PreDestroy
     public void preDestroyPrint() {
-        System.out.println("MyServiceImpl.preDestroyPrint() at " + LocalDateTime.now().toString());
+        log.trace("MyServiceImpl.preDestroyPrint() at {}", LocalDateTime.now().toString());
     }
 }
